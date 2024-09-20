ba8549a03848b02f4546eb7489f22aa6222bb2ec
adc77 <amandogra2016@gmail.com>
2024-09-20 07:58:19 +0530

    ip-op added

--- esrgan.py
+++ esrgan.py
@@ -8,7 +8,7 @@ device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 model = RealESRGAN(device, scale=4)
 model.load_weights('weights/RealESRGAN_x4.pth', download=True)
 
-path_to_image = 'examples/example_9.jpg'
+path_to_image = 'inputs\input4.jpg'
 image = Image.open(path_to_image).convert('RGB')
 
 sr_image = model.predict(image)
